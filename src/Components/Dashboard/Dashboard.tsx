b82792033561fab5a9d30960758199520512c954
asokaravindap <asok.perera@cevo.com.au>
2024-05-23 21:59:05 +1000

    move from prime react to material UI

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,15 +1,29 @@
-import "primereact/resources/themes/saga-orange/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css";
-
 import { useEffect, useState } from "react";
-import CustomerTable from "./CustomerTable/CustomerTable";
-import SidePane from "./SidePane/SidePane";
 
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { AuthSession, fetchAuthSession } from "aws-amplify/auth";
+
+import CustomerTable from "./CustomerTable/CustomerTable";
+import SidePane from "./SidePane/SidePane";
 import NewCustomer from "./NewCustomer/NewCustomer";
+
+const containerStyle = {
+  display: "flex",
+  width: "100vw",
+  height: "100vh",
+};
+
+const sidePane = {
+  display: "flex",
+  alignItems: "center",
+  paddingLeft: "20px",
+  paddingRight: "20px",
+};
+
+const board = {
+  // flex: 1,
+  // backgroundColor: "yellow",
+};
 
 const Dashboard = () => {
   const [session, setSession] = useState<AuthSession>({});
@@ -26,63 +40,17 @@ const Dashboard = () => {
     setSession(currentSession);
     setIsLoading(false);
   }
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        backgroundColor: "var(--surface-300)",
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: "var(--primary-color)",
-          borderRadius: "0 0 30px 0",
-          top: "0",
-          width: "100vw",
-          flex: 1,
-        }}
-      >
-        Header
-      </div>
-      <div
-        style={{
-          flex: 10,
-          backgroundColor: "var(--surface-300)",
-          width: "100vw",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <div
-            style={{
-              flex: 1,
-              padding: "1rem 1rem 1rem 1rem",
-              borderRadius: "0 30px 30px 0",
-              backgroundColor: "var(--surface-100)",
-            }}
-          >
-            <SidePane signOut={signOut!}></SidePane>
-          </div>
-
+    <div style={{ backgroundColor: "white" }}>
+      <div style={containerStyle}>
+        <div style={sidePane}>
+          <SidePane signOut={signOut!}></SidePane>
+        </div>
+        <div style={board}>
           {isLoading ? (
             <div>Loading...</div>
           ) : (
-            <div
-              style={{
-                flex: 7,
-                padding: "2rem 2rem 2rem 2rem",
-                overflowY: "auto",
-              }}
-            >
+            <div>
               <NewCustomer session={session} user={user}></NewCustomer>
               <CustomerTable session={session} user={user}></CustomerTable>
             </div>
